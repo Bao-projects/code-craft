94f52927203b64417630be0e565f1912574e21cc
BaoThan <109579681+BaoThan@users.noreply.github.com>
2023-10-07 14:19:55 -0700

    Implement_execute_API (#3)
    Implement a  REST API enabling users to call the code executor.

---
+++ code_craft/routes/helper/responses.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from flask import jsonify
+from flask import Response
+
+
+@dataclass
+class ExecutionResult:
+    output: str
+    exit_code: int
+
+    def to_flask_response(self) -> tuple[Response, int]:
+        return (
+            jsonify(
+                {
+                    "success": True,
+                    "data": {
+                        "output": self.output,
+                        "exit_code": self.exit_code,
+                    },
+                }
+            ),
+            200,
+        )
+
+
+@dataclass
+class FailureResult:
+    error_messsage: str
+
+    def to_flask_response(self) -> tuple[Response, int]:
+        return jsonify({"success": False, "error_message": self.error_messsage}), 400

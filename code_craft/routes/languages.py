7c44f7572a28a3785ed98716618d4efff159b1d7
Bao Than <b.than@sfsu.edu>
2023-10-15 00:21:47 -0700

    Add flask cors

--- code_craft/routes/languages.py
+++ code_craft/routes/languages.py
@@ -1,10 +1,12 @@
 from flask import jsonify
+from flask_cors import cross_origin
 
 from . import routes
 from code_craft.code_executor import Language
 
 
 @routes.route("/languages")
+@cross_origin()
 def languages():
     """
     GET API to get a list of supported programming languages.
